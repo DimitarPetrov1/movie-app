673c2f500c95b1cb0154056bd011f52f28d739d4
Dimitar Petrov <dimitarpetrov1990@gmail.com>
2021-03-08 00:09:25 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,39 +1,103 @@
 import React, { useState, useEffect } from "react";
 import AppHeader from "./AppHeader";
 import TopCarousel from "./TopCarousel";
-import MoviesEntry from "./MoviesEntry";
+import Star from "./svg/star.svg";
 
 import axios from "axios";
 
-function App(props) {
-  const [entries, setEntries] = useState([]);
+function App({ movieTitle, movieImg, movieYear, movieRating }) {
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [discoverMovies, setDiscoverMovies] = useState([]);
   const API_KEY = "5d0bfc21ffb33bbfd548ebe383da65cc";
+  const MAIN_URL = "https://api.themoviedb.org/3/";
+  const IMG_URL = "https://image.tmdb.org/t/p/original/";
+
   useEffect(() => {
-    async function fetchData() {
-      const reqeust = await axios.get(
-        `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
+    async function fetchTopRated() {
+      const request = await axios.get(
+        `${MAIN_URL}movie/top_rated?api_key=${API_KEY}`
       );
-      setEntries(reqeust.data.results);
-      return reqeust;
+      setTopRatedMovies(request.data.results);
+      return request;
     }
-    fetchData();
+    async function fetchDiscover() {
+      const request = await axios.get(
+        `${MAIN_URL}discover/movie?api_key=${API_KEY}`
+      );
+      setDiscoverMovies(request.data.results);
+      return request;
+    }
+    fetchTopRated();
+    fetchDiscover();
     // need to add sth later (the orl from variable)
   }, []);
 
-  const IMG_URL = "https://image.tmdb.org/t/p/original/";
+  console.log(discoverMovies);
 
   return (
     <div className="app">
       <AppHeader />
       <div className="container-main">
+        <h2 className="heading-title">Trending now</h2>
         <TopCarousel />
+        <div className="sorting-wrap">
+          <input type="search" placeholder="Search" />
+          <select defaultValue="" name="genre">
+            <option value="undefined" selected>
+              Genre
+            </option>
+            <option value="">Horror</option>
+            <option value="">Comedy</option>
+            <option value="">Action</option>
+            <option value="">Fantasy</option>
+          </select>
+
+          <select defaultValue="" name="year">
+            <option value="undefined" selected>
+              Year
+            </option>
+            <option value="">2020</option>
+            <option value="">2019</option>
+            <option value="">2018</option>
+            <option value="">2017</option>
+            <option value="">2016</option>
+          </select>
+
+          <select defaultValue="" name="rating">
+            <option value="undefined" selected>
+              Rating
+            </option>
+            <option value="">9+</option>
+            <option value="">8+</option>
+            <option value="">7+</option>
+            <option value="">6+</option>
+            <option value="">5+</option>
+            <option value="">4+</option>
+            <option value="">3+</option>
+            <option value="">2+</option>
+            <option value="">1+</option>
+          </select>
+          <input className="btn btn-submit" type="submit" value="Submit" />
+        </div>
+        <h2 className="heading-title">Top rated</h2>
+
         <ul className="movie-list">
-          {entries.map((entry) => {
+          {topRatedMovies.map((entry) => {
             return (
-              <MoviesEntry
-                movieTitle={entry.original_title}
-                movieImg={`${IMG_URL}${entry.poster_path}`}
-              />
+              <li key={entry.id}>
+                <img
+                  src={`${IMG_URL}${entry.poster_path}`}
+                  alt={`Movie: ${entry.original_title}`}
+                />
+                <div className="movie-extra">
+                  <p className="movie-year">{entry.release_date}</p>
+                  <p className="movie-rating">
+                    <img className="star" src={Star} alt="star" />
+                    &nbsp;{entry.vote_average}
+                  </p>
+                  {/* <p className="movie-genre">{movieGenre}</p> */}
+                </div>
+              </li>
             );
           })}
         </ul>
--- src/TopCarousel.js
+++ src/TopCarousel.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import CarouselEntry from "./CarouselEntry";
+
+import axios from "axios";
 
 function TopCarousel(props) {
+  const [entries, setEntries] = useState([]);
+  const API_KEY = "5d0bfc21ffb33bbfd548ebe383da65cc";
+  useEffect(() => {
+    async function fetchData() {
+      const reqeust = await axios.get(
+        `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`
+      );
+      setEntries(reqeust.data.results);
+      return reqeust;
+    }
+    fetchData();
+    // need to add sth later (the orl from variable)
+  }, []);
+
+  const IMG_URL = "https://image.tmdb.org/t/p/original/";
+
   return (
     <div className="carrousel">
       <input type="radio" name="slides" id="radio-1" defaultChecked />
@@ -10,20 +27,53 @@ function TopCarousel(props) {
       <input type="radio" name="slides" id="radio-3" />
       <input type="radio" name="slides" id="radio-4" />
       <ul className="slides">
-        {/* {data.splice(0, 4).map((entry) => {
+        {entries.map((entry) => {
           return (
-            <CarouselEntry
-              key={entry.movieTitle}
-              movieImgSecondary={entry.movieImgSecondary}
-              movieTitle={entry.movieTitle}
-              movieDescription={entry.movieDesc}
-              movieYear={entry.movieYear}
-              movieRating={entry.movieRating}
-              movieGenre={entry.movieGenre}
-              movieDuration={entry.movieDuration}
-            />
+            <li className="slide" key={entry.id}>
+              <div className="slide-img-wrap">
+                <img
+                  src={`${IMG_URL}${entry.backdrop_path}`}
+                  alt={`Movie: ${entry.original_title}`}
+                />
+              </div>
+              <div className="slide-details">
+                <h2 className="car-movie-title">{entry.original_title}</h2>
+                {/* Make a function for max charakters e.g 30 then ... */}
+                <div className="car-movie-description">{entry.overview}</div>
+                <div className="car-detail car-movie-year">
+                  <div>Year:</div>
+                  {entry.release_date}
+                </div>
+                <div className="car-detail car-movie-rating">
+                  <div>Rating:</div>
+                  <svg
+                    width="14"
+                    height="14"
+                    viewBox="0 0 24 24"
+                    fill="#54426b"
+                    stroke="#54426b"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-star"
+                  >
+                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+                  </svg>
+                  &nbsp;{entry.vote_average}/10
+                </div>
+                {/* <div className="car-detail car-movie-genre">
+                <div>Genre:</div>
+                {movieGenre}
+              </div> */}
+                {/* <div className="car-detail car-movie-duration">
+                  <div>Duration:</div>
+                  {movieDuration}min
+                </div> */}
+                <button className="btn btn-play btn-carousel">Play now</button>
+              </div>
+            </li>
           );
-        })} */}
+        })}
       </ul>
       <div className="slidesNavigation">
         <label htmlFor="radio-1" id="dotForRadio-1" />
