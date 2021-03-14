cbc949aca7e918041a10fb58a72268bbddbc0e7a
Dimitar Petrov <60468978+DimitarPetrov1@users.noreply.github.com>
2021-03-14 22:44:05 +0200

    added modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import AppHeader from "./AppHeader";
 import TopCarousel from "./TopCarousel";
 import Star from "./svg/star.svg";
+import Modal from "./Modal";
 
-import axios from "axios";
-
-function App({ movieTitle, movieImg, movieYear, movieRating }) {
-  const [curentTitles, setCurentTitles] = useState([]);
+function App(props) {
   const [defaultTitles, setDefaultTitles] = useState("movie/top_rated");
+  const [curentTitles, setCurentTitles] = useState([]);
   const [searchStr, setSearchStr] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const [modalDetails, setModalDetails] = useState({});
   const API_KEY = "5d0bfc21ffb33bbfd548ebe383da65cc";
   const MAIN_URL = "https://api.themoviedb.org/3/";
   const IMG_URL = "https://image.tmdb.org/t/p/original/";
   const IMG_PLACEHOLDER =
-    "https://www.c-capture.co.uk/wp/wp-content/uploads/portrait-placeholder.jpg";
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png";
 
   useEffect(() => {
     async function fetchCurentTitles() {
@@ -33,25 +35,53 @@ function App({ movieTitle, movieImg, movieYear, movieRating }) {
     // need to add sth later (the orl from variable)
   }, [defaultTitles, searchStr]);
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchStr(e.target.value);
+  };
+
   const handleChange = (e) => {
+    e.preventDefault();
     setDefaultTitles(e.target.value);
   };
 
-  const handleSearch = (e) => {
-    setSearchStr(e.target.value);
+  const handleOpenCloseModal = (e) => {
+    const cont = document.querySelector(".app");
+    const modalWrap = document.querySelector(".modal-wrap");
+    cont.style.height = "100vh";
+    cont.style.overflowY = "hidden";
+    setOpenModal(true);
+    modalDetails.header = e.target.alt;
+    modalDetails.body = e.target.getAttribute("data-body");
+    modalDetails.image = e.target.src;
+    modalWrap.addEventListener("click", (e) => {
+      if (e.target.classList.contains("modal-wrap")) {
+        setOpenModal(false);
+        cont.style.height = "unset";
+        cont.style.overflowY = "unset";
+      } else {
+        return null;
+      }
+    });
   };
+
   return (
     <div className="app">
       <AppHeader />
       <div className="container-main">
+        <Modal
+          openModalConponent={openModal}
+          modalHeader={modalDetails.header}
+          modalBody={modalDetails.body}
+          modalImage={modalDetails.image}
+        />
         <p className="heading-title">Trending this week</p>
-        <TopCarousel />
+        {/* <TopCarousel /> */}
         <div className="sorting-wrap">
           <input type="search" placeholder="Search" onChange={handleSearch} />
           <select
             defaultValue="movie/top_rated"
             id="currentTitles"
-            name="fdf"
             onChange={handleChange}
           >
             <option value="movie/top_rated">Top rated</option>
@@ -64,10 +94,11 @@ function App({ movieTitle, movieImg, movieYear, movieRating }) {
         </label> */}
 
         <ul className="movie-list">
-          {curentTitles.map((entry) => {
+          {curentTitles.slice(0, 20).map((entry) => {
             return (
-              <li key={entry.id}>
+              <li key={entry.id} onClick={handleOpenCloseModal}>
                 <img
+                  data-body={entry.overview}
                   loading="lazy"
                   src={
                     entry.poster_path
@@ -75,13 +106,14 @@ function App({ movieTitle, movieImg, movieYear, movieRating }) {
                       : // : `${IMG_URL}${entry.backdrop_path}`
                         IMG_PLACEHOLDER
                   }
+                  className="movie-list__img"
                   alt={`Movie: ${entry.original_title}`}
                 />
                 <div className="movie-extra">
                   <p className="movie-year">
                     {entry.release_date
                       ? entry.release_date.substr(0, 4)
-                      : entry.release_date}
+                      : "n/a"}
                   </p>
                   <p className="movie-rating">
                     <img className="star" src={Star} alt="star" />
---
+++ src/Modal.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./modal.css";
+
+function Modal({ openModalConponent, modalHeader, modalBody, modalImage }) {
+  return (
+    <div
+      className={openModalConponent ? "modal-wrap modal-open" : "modal-wrap"}
+    >
+      <div className="modal_component">
+        <img className="modal-img" src={modalImage} alt={modalHeader} />
+        <div className="modal-info">
+          <p className="modal-header">{modalHeader}</p>
+          <p className="modal-body">{modalBody}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
