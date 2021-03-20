6a9048df825971bcc42c10cdc8140d016e8ed4cc
Dimitar Petrov <60468978+DimitarPetrov1@users.noreply.github.com>
2021-03-20 19:02:43 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { API_KEY, MAIN_URL, IMG_URL, IMG_PLACEHOLDER } from "./vars";
+import { API_KEY, MAIN_URL, IMG_PLACEHOLDER, IMG_URL_MEDIUM } from "./vars";
 import AppHeader from "./AppHeader";
 import TopCarousel from "./TopCarousel";
 import Star from "./svg/star.svg";
@@ -8,55 +8,67 @@ import Modal from "./Modal";
 
 function App(props) {
   const [openModal, setOpenModal] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [searchStr, setSearchStr] = useState("");
+  const [category, setCategory] = useState("");
   const [currentTitles, setCurrentTitles] = useState([]);
+  const [modalDetails, setModalDetails] = useState([]);
+
+  const top_rated = `${MAIN_URL}movie/top_rated?api_key=${API_KEY}`;
+  let var_category = `${MAIN_URL}${category}?api_key=${API_KEY}`;
+  const searching = `${MAIN_URL}search/movie?api_key=${API_KEY}&query=${searchStr}`;
+
+  // const fetchVideoKey = async () => {
+  //   const keyRequest = await axios
+  //     .get(`${MAIN_URL}movie/123/videos?api_key=${API_KEY}`)
+  //     .then((data) => {
+  //       setModalDetails.video = data.data.results[0].key;
+  //     })
+  //     .catch((err) => console.log(err));
+  //   return keyRequest;
+  // };
 
   useEffect(() => {
-    async function fetchTitles() {
-      const getTopRated = await axios.get(
-        `${MAIN_URL}movie/top_rated?api_key=${API_KEY}`
-      );
-      setCurrentTitles(getTopRated.data.results);
-    }
+    const fetchTitles = async () => {
+      // if we search for something we use the searching const as string
+      // if we don't search for anything there are 2 things that need to be done:
+      // 1. we get the default top rated to display some items in the grid
+      // 2. if we change the category then fetch that category and display it
+      if (searchStr === "") {
+        if (category === "") {
+          const req = await axios.get(top_rated).then((data) => {
+            setCurrentTitles(data.data.results);
+          });
+          return req;
+        } else {
+          const req = await axios.get(var_category).then((data) => {
+            setCurrentTitles(data.data.results);
+          });
+          return req;
+        }
+      } else {
+        const req = await axios.get(searching).then((data) => {
+          setCurrentTitles(data.data.results);
+        });
+        return req;
+      }
+    };
+
     fetchTitles();
-  }, []);
+    // fetchVideoKey();
 
-  console.log(currentTitles);
-
-  // useEffect(() => {
-  //   async function fetchTitles() {
-  //     // Start the request when loading > If nothing is searched fetch the default
-  //     // If there is a search fetch the search only
-  //     const request = await axios
-  //       .get(
-  //         searchStr === ""
-  //           ? `${MAIN_URL}${initialTitles}?api_key=${API_KEY}`
-  //           : `${MAIN_URL}search/movie?api_key=${API_KEY}&query=${searchStr}`
-  //       )
-  //       .catch((err) => console.log(err));
-  //     setCurrentTitles(request.data.results);
-  //     return request;
-  //   }
-  //   async function fetchVideoKey() {
-  //     const keyRequest = await axios
-  //       .get(`${MAIN_URL}movie/123/videos?api_key=${API_KEY}`)
-  //       .catch((err) => console.log(err));
-  //     setModalDetails.video = keyRequest.data.results[0].key;
-  //     return keyRequest;
-  //   }
-  //   fetchTitles();
-  //   fetchVideoKey();
-
-  //   // need to add sth later (the orl from variable)
-  // }, [initialTitles, searchStr]);
+    setLoading(false);
+  }, [currentTitles]);
 
   const handleSearch = (e) => {
     e.preventDefault();
-    // setSearchStr(e.target.value);
+    setSearchStr(e.target.value);
   };
 
   const handleChange = (e) => {
     e.preventDefault();
-    // setInitialTitles(e.target.value);
+    setSearchStr("");
+    setCategory(e.target.value);
   };
 
   const handleOpenCloseModal = (e) => {
@@ -64,11 +76,11 @@ function App(props) {
     const modalWrap = document.querySelector(".modal-wrap");
     cont.classList.add("modal-clear");
     setOpenModal(true);
-    // modalDetails.id = parseInt(e.target.getAttribute("data-id"));
-    // modalDetails.header = e.target.alt;
-    // modalDetails.body = e.target.getAttribute("data-body");
-    // modalDetails.image = e.target.src;
-    // modalDetails.video = e.target.getAttribute("data-id");
+    modalDetails.id = Number(e.target.getAttribute("data-id"));
+    modalDetails.header = e.target.alt;
+    modalDetails.body = e.target.getAttribute("data-body");
+    modalDetails.image = e.target.src;
+    modalDetails.video = e.target.getAttribute("data-id");
     modalWrap.addEventListener("click", (e) => {
       if (e.target.classList.contains("modal-wrap")) {
         setOpenModal(false);
@@ -82,21 +94,27 @@ function App(props) {
     <div className="app">
       <AppHeader />
       <div className="container-main">
-        {/* <Modal
+        <Modal
           openModalConponent={openModal}
           modalHeader={modalDetails.header}
           modalBody={modalDetails.body}
           modalImage={modalDetails.image}
           modalVideo={modalDetails.video}
-        /> */}
+        />
         <p className="heading-title">Trending this week</p>
         {/* <TopCarousel /> */}
         <div className="sorting-wrap">
-          <input type="search" placeholder="Search" onChange={handleSearch} />
+          <input
+            value={searchStr ? searchStr : ""}
+            type="search"
+            placeholder="Search movies"
+            onChange={handleSearch}
+          />
           <select
             defaultValue="movie/top_rated"
             id="currentTitles"
             onChange={handleChange}
+            disabled={searchStr ? true : false}
           >
             <option value="movie/top_rated">Top rated</option>
             <option value="trending/all/day">Trending (Today)</option>
@@ -106,40 +124,43 @@ function App(props) {
         {/* <label htmlFor="currentTitles" className="heading-title">
           Top rated
         </label> */}
-
-        <ul className="movie-list">
-          {currentTitles.slice(0, 10).map((entry) => {
-            return (
-              <li key={entry.id} onClick={handleOpenCloseModal}>
-                <img
-                  data-body={entry.overview}
-                  data-id={entry.id}
-                  loading="lazy"
-                  src={
-                    entry.poster_path
-                      ? `${IMG_URL}${entry.poster_path}`
-                      : // : `${IMG_URL}${entry.backdrop_path}`
-                        IMG_PLACEHOLDER
-                  }
-                  className="movie-list__img"
-                  alt={`Movie: ${entry.original_title}`}
-                />
-                <div className="movie-extra">
-                  <p className="movie-year">
-                    {entry.release_date
-                      ? entry.release_date.substr(0, 4)
-                      : "n/a"}
-                  </p>
-                  <p className="movie-rating">
-                    <img className="star" src={Star} alt="star" />
-                    &nbsp;{entry.vote_average}
-                  </p>
-                  {/* <p className="movie-genre">{movieGenre}</p> */}
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        {!isLoading ? (
+          <ul className="movie-list">
+            {currentTitles.slice(0, 20).map((entry) => {
+              return (
+                <li key={entry.id} onClick={handleOpenCloseModal}>
+                  <img
+                    data-body={entry.overview}
+                    data-id={entry.id}
+                    loading="lazy"
+                    src={
+                      entry.poster_path
+                        ? `${IMG_URL_MEDIUM}${entry.poster_path}`
+                        : // : `${IMG_URL}${entry.backdrop_path}`
+                          IMG_PLACEHOLDER
+                    }
+                    className="movie-list__img"
+                    alt={`Movie: ${entry.original_title}`}
+                  />
+                  <div className="movie-extra">
+                    <p className="movie-year">
+                      {entry.release_date
+                        ? entry.release_date.substr(0, 4)
+                        : "n/a"}
+                    </p>
+                    <p className="movie-rating">
+                      <img className="star" src={Star} alt="star" />
+                      &nbsp;{entry.vote_average}
+                    </p>
+                    {/* <p className="movie-genre">{movieGenre}</p> */}
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
--- src/vars.js
+++ src/vars.js
@@ -1,7 +1,8 @@
 const API_KEY = "5d0bfc21ffb33bbfd548ebe383da65cc";
 const MAIN_URL = "https://api.themoviedb.org/3/";
-const IMG_URL = "https://image.tmdb.org/t/p/original/";
+const IMG_URL_ORIGINAL = "https://image.tmdb.org/t/p/original/";
+const IMG_URL_MEDIUM = "https://image.tmdb.org/t/p/w500/";
 const IMG_PLACEHOLDER =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png";
 
-export { API_KEY, MAIN_URL, IMG_URL, IMG_PLACEHOLDER };
+export { API_KEY, MAIN_URL, IMG_PLACEHOLDER, IMG_URL_ORIGINAL, IMG_URL_MEDIUM };
